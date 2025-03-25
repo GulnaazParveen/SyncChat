1458e2c801ca2047e15097f7d75d4ca48a3ee0fd
Gulnaaz <gulnaazparweenpap@gmail.com>
2025-03-25 18:00:51 +0530

    Removed manual CORS headers, using cors middleware

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,6 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// CORS Middleware
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
